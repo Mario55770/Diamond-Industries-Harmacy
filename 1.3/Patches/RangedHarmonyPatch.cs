abc96df9830aa759fd472532db2758499f76eefa
Mario55770 <feretty@outlook.com>
2022-02-09 12:48:09 -0500

    Commented and code cleanup in a refactor. ToDo test it.

--- 1.3/Patches/RangedHarmonyPatch.cs
+++ 1.3/Patches/RangedHarmonyPatch.cs
@@ -7,13 +7,11 @@ using HarmonyLib;
 namespace DI_Harmacy
 {
     [StaticConstructorOnStartup]
-    [HarmonyDebug]
     class RangedHarmonyPatch
     {
         static ExtraDamage poisonDamage;
         static RangedHarmonyPatch()
         {
-            // Log.Message("TEST LAUNCH ERROR");
             DoPatching();
         }
 
@@ -22,46 +20,43 @@ namespace DI_Harmacy
             var harmony = new Harmony("DI_Harmacy.PoisonProjectileLaunch.patch");
             var rOriginal = AccessTools.Method(typeof(Projectile), "Launch", new[] { typeof(Thing), typeof(Vector3), typeof(LocalTargetInfo), typeof(LocalTargetInfo), typeof(ProjectileHitFlags), typeof(bool), typeof(Thing), typeof(ThingDef) });
             Thing variables = null;
-            Def nullDef = null;
             Projectile p = null;
             var rPostFix = SymbolExtensions.GetMethodInfo(() => rangedPoisonPostFix(variables, variables, ref p));
             harmony.Patch(rOriginal, null, new HarmonyMethod(rPostFix));
+            //Creates a damage def once so we don't keep making it.
             ExtraDamage poisonDamage = new ExtraDamage
             {
                 chance = 1f,
                 amount = 1f
             };
             poisonDamage.armorPenetration = 1f;
-
-
             DamageDef poisonDamageDef = DIH_DamageDefs.DIH_PoisonDamageBase;
-
             poisonDamageDef.isRanged = true;
-
             poisonDamage.def = poisonDamageDef;
         }
 
         public static void rangedPoisonPostFix(Thing launcher, Thing equipment, ref Projectile __instance)
         {
+            //gets comp, checks if null, has no hediff, or cannot be used. Ends method if so.
             CompPoisonable compPoisonable = equipment.TryGetComp<CompPoisonable>();
             if (compPoisonable == null || compPoisonable.Props.hediffToApply == null || compPoisonable.CanBeUsed == false)
+            {
                 return;
-           
-           
-
+            }
+            //checks if list is null or empty. If so, make my own list for this.
             if (__instance.def.projectile.extraDamages.NullOrEmpty<ExtraDamage>())
             {
-                if (__instance.def.projectile.extraDamages == null)
-                {
+               //if (__instance.def.projectile.extraDamages == null)
+                //{
                     
                     ExtraDamage[] extradamageArr = { poisonDamage };
                     List<ExtraDamage> extraDamages = new List<ExtraDamage>(extradamageArr);
                     __instance.def.projectile.extraDamages = extraDamages;
-                }
+                //}
             }
             else
             {
-              
+                //if a extra damage with my damage def does not exist, add my own damage def. Slower than assuming it will either not exist or will have my damage def...but safer
                 foreach (ExtraDamage exDamage in __instance.def.projectile.extraDamages)
                 {
                     if (exDamage.def.Equals(poisonDamage.def))

d35df698100b15d1908dc072a336f808830a7713
Mario55770 <feretty@outlook.com>
2022-02-09 12:01:42 -0500

    Working cleanup in progress version. no more duplicate extra damages.

--- 1.3/Patches/RangedHarmonyPatch.cs
+++ 1.3/Patches/RangedHarmonyPatch.cs
@@ -22,17 +22,17 @@ namespace DI_Harmacy
             var rOriginal = AccessTools.Method(typeof(Projectile), "Launch", new[] { typeof(Thing), typeof(Vector3), typeof(LocalTargetInfo), typeof(LocalTargetInfo), typeof(ProjectileHitFlags), typeof(bool), typeof(Thing), typeof(ThingDef) });
             Thing variables = null;
             Def nullDef = null;
-             Projectile p = null;
-            var rPostFix = SymbolExtensions.GetMethodInfo(() => rangedPoisonPostFix(variables, variables,ref p));
+            Projectile p = null;
+            var rPostFix = SymbolExtensions.GetMethodInfo(() => rangedPoisonPostFix(variables, variables, ref p));
             harmony.Patch(rOriginal, null, new HarmonyMethod(rPostFix));
         }
-        // 
+
         public static void rangedPoisonPostFix(Thing launcher, Thing equipment, ref Projectile __instance)
         {
-           CompPoisonable compPoisonable= equipment.TryGetComp<CompPoisonable>();
-            if (compPoisonable == null || compPoisonable.Props.hediffToApply == null ||compPoisonable.CanBeUsed==false)
+            CompPoisonable compPoisonable = equipment.TryGetComp<CompPoisonable>();
+            if (compPoisonable == null || compPoisonable.Props.hediffToApply == null || compPoisonable.CanBeUsed == false)
                 return;
-                Log.Message("RangedPoison");
+           
             ExtraDamage poisonDamage = new ExtraDamage
             {
                 chance = 1f,
@@ -40,33 +40,38 @@ namespace DI_Harmacy
             };
             poisonDamage.armorPenetration = 1f;
 
-            //DamageInfo dInfo=new DamageInfo(DIH_DamageDefs.DIH_PoisonDamageBase, poisonDamage.amount, instigator: launcher);
-            
+
             DamageDef poisonDamageDef = DIH_DamageDefs.DIH_PoisonDamageBase;
-            //poisonDamageDef
+
             poisonDamageDef.isRanged = true;
-            
+
             poisonDamage.def = poisonDamageDef;
-            // Log.Message(poisonDamage.AdjustedDamageAmount().ToString());
-            
+
             if (__instance.def.projectile.extraDamages.NullOrEmpty<ExtraDamage>())
+            {
+                if (__instance.def.projectile.extraDamages == null)
                 {
-                    Log.Message("List is null or empty");
-                if(__instance.def.projectile.extraDamages==null)
-                    { Log.Message("List seems null");
+                    
                     ExtraDamage[] extradamageArr = { poisonDamage };
                     List<ExtraDamage> extraDamages = new List<ExtraDamage>(extradamageArr);
                     __instance.def.projectile.extraDamages = extraDamages;
-                }   
                 }
-            else {
-                Log.Message(__instance.def.projectile.extraDamages.Count().ToString());
+            }
+            else
+            {
+              
+                foreach (ExtraDamage exDamage in __instance.def.projectile.extraDamages)
+                {
+                    if (exDamage.def.Equals(poisonDamage.def))
+                    {
+
+                        return;
+                    }
+                }
                 __instance.def.projectile.extraDamages.Add(poisonDamage);
             }
-            Log.Message(__instance.def.projectile.extraDamages.Count().ToString());
-            Log.Message("Added to list");
-           // Log.Message(__instance.def.projectile.extraDamages.Last<ExtraDamage>().toString());
-            
+           
+
         }
     }
 }

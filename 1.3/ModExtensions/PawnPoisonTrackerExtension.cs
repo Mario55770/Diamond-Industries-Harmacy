828e1daec0afe8d8a4e0bcf55ca3695b4bae868c
Mario55770 <feretty@outlook.com>
2022-02-14 13:59:17 -0500

    Working Swap to medicine dropdown. Swap to poison dropdown not yet implemented

--- 1.3/ModExtensions/PawnPoisonTrackerExtension.cs
+++ 1.3/ModExtensions/PawnPoisonTrackerExtension.cs
@@ -8,6 +8,7 @@ namespace DI_Harmacy
     class PawnPoisonTrackerExtension:DefModExtension
     {
         public bool test;
-        public Pawn_PoisonPolicyTracker pawn_PoisonPolicyTracker=new Pawn_PoisonPolicyTracker();
+        //public Pawn_PoisonPolicyTracker pawn_PoisonPolicyTracker=new Pawn_PoisonPolicyTracker();
+        public ThingDef poisonToUse=null;
     }
 }

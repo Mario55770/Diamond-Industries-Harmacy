53f35939837dcfe237c43d1cb000f51a782f81c6
Mario55770 <feretty@outlook.com>
2022-02-15 13:29:34 -0500

    Desynced Medicine Menu

---
+++ 1.3/Comps/CompPawnPoisonTracker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using RimWorld;
+namespace DI_Harmacy
+{
+    public class CompPawnPoisonTracker : ThingComp
+    {
+        public override void CompTick()
+        {
+            //Log.Error("Let's error on every tick!");
+        }
+        public Pawn_InventoryStockTracker pawn_InventoryStockTracker = null;
+        public CompPropertiesPawnPoisonTracker Props => (CompPropertiesPawnPoisonTracker)this.props;
+
+        }
+
+    public class CompPropertiesPawnPoisonTracker : CompProperties
+    {
+        
+        public CompPropertiesPawnPoisonTracker()
+        {
+            this.compClass = typeof(CompPawnPoisonTracker);
+        }
+
+        public CompPropertiesPawnPoisonTracker(Type compClass) : base(compClass)
+        {
+            this.compClass = compClass;
+        }
+    }
+}
+
--- 1.3/UI/PawnColumnWorker_Poison.cs
+++ 1.3/UI/PawnColumnWorker_Poison.cs
@@ -12,26 +12,46 @@ namespace DI_Harmacy
 
 		public override void DoCell(Rect rect, Pawn pawn, PawnTable table)
 		{
-			if (pawn.inventoryStock != null)
+			CompPawnPoisonTracker c = pawn.GetComp<CompPawnPoisonTracker>();
+			if(c==null)
+            {
+				return;
+            }
+			if (c.pawn_InventoryStockTracker==null)
+			{
+				c.pawn_InventoryStockTracker = new Pawn_InventoryStockTracker(pawn);
+			}
+			Pawn_InventoryStockTracker pawn_InventoryStockTracker = c.pawn_InventoryStockTracker;
+			//formerly pawn.inventory
+			if (pawn_InventoryStockTracker != null)
 			{
 				float num = rect.width - 4f;
 				int num2 = Mathf.FloorToInt(num * 0.333333343f);
 				float x = rect.x;
 				InventoryStockGroupDef group = InventoryStockGroupDefOf.Medicine;
-				Widgets.Dropdown(new Rect(x, rect.y + 2f, num2, rect.height - 4f), pawn, (Pawn p) => p.inventoryStock.GetDesiredThingForGroup(group), (Pawn p) => GenerateThingButtons(p, group), null, pawn.inventoryStock.GetDesiredThingForGroup(group).uiIcon, null, null, null, paintable: true);
-				//Widgets.Dropdown(new Rect(x + (float)num2 + 4f, width: Mathf.FloorToInt(num * (2f / 3f)), y: rect.y + 2f, height: rect.height - 4f), pawn, (Pawn p) => p.inventoryStock.GetDesiredCountForGroup(group), (Pawn p) => GenerateCountButtons(p, group), pawn.inventoryStock.GetDesiredCountForGroup(group).ToString(), null, null, null, null, paintable: true);
+				//InventoryStockGroupDef group=DIH_PoisonStockGroups.DIH_PoisonStockGroup
+				/**InventoryStockGroupDef group = new InventoryStockGroupDef();
+					group.thingDefs = PoisonItemFinder.poisonItemList;
+				group.max = 3;*/
+				
+				Widgets.Dropdown(new Rect(x, rect.y + 2f, num2, rect.height - 4f), pawn, (Pawn p) => p.inventoryStock.GetDesiredThingForGroup(group), (Pawn p) => GenerateThingButtons(p, group), null, pawn_InventoryStockTracker.GetDesiredThingForGroup(group).uiIcon, null, null, null, paintable: true);
+				Widgets.Dropdown(new Rect(x + (float)num2 + 4f, width: Mathf.FloorToInt(num * (2f / 3f)), y: rect.y + 2f, height: rect.height - 4f), pawn, (Pawn p) => p.inventoryStock.GetDesiredCountForGroup(group), (Pawn p) => GenerateCountButtons(p, group), pawn_InventoryStockTracker.GetDesiredCountForGroup(group).ToString(), null, null, null, null, paintable: true);
 			}
 		}
 
 		private IEnumerable<Widgets.DropdownMenuElement<ThingDef>> GenerateThingButtons(Pawn pawn, InventoryStockGroupDef group)
 		{
+			CompPawnPoisonTracker c = pawn.GetComp<CompPawnPoisonTracker>();
+			Pawn_InventoryStockTracker pawn_InventoryStockTracker = c.pawn_InventoryStockTracker;
 			foreach (ThingDef thing in group.thingDefs)
 			{
 				yield return new Widgets.DropdownMenuElement<ThingDef>
 				{
 					option = new FloatMenuOption(thing.LabelCap, delegate
 					{
-						pawn.inventoryStock.SetThingForGroup(group, thing);
+						//this line updates the pawn to change its tracker
+						//pawn.def.GetModExtension<PawnPoisonTrackerExtension>().poisonToUse = thing;
+						pawn_InventoryStockTracker.SetThingForGroup(group, thing);
 					}),
 					payload = thing
 				};
@@ -40,6 +60,8 @@ namespace DI_Harmacy
 
 		private IEnumerable<Widgets.DropdownMenuElement<int>> GenerateCountButtons(Pawn pawn, InventoryStockGroupDef group)
 		{
+			CompPawnPoisonTracker c = pawn.GetComp<CompPawnPoisonTracker>();
+			Pawn_InventoryStockTracker pawn_InventoryStockTracker = c.pawn_InventoryStockTracker;
 			for (int i = group.min; i <= group.max; i++)
 			{
 				int localI = i;
@@ -47,7 +69,7 @@ namespace DI_Harmacy
 				{
 					option = new FloatMenuOption(i.ToString(), delegate
 					{
-						pawn.inventoryStock.SetCountForGroup(group, localI);
+						pawn_InventoryStockTracker.SetCountForGroup(group, localI);
 					}),
 					payload = i
 				};
@@ -76,9 +98,11 @@ namespace DI_Harmacy
 
 		private int GetValueToCompare(Pawn pawn)
 		{
-			if (pawn.inventoryStock != null)
+			CompPawnPoisonTracker c = pawn.GetComp<CompPawnPoisonTracker>();
+			Pawn_InventoryStockTracker pawn_InventoryStockTracker = c.pawn_InventoryStockTracker;
+			if (pawn_InventoryStockTracker!= null)
 			{
-				return pawn.inventoryStock.GetDesiredCountForGroup(InventoryStockGroupDefOf.Medicine);
+				return pawn_InventoryStockTracker.GetDesiredCountForGroup(InventoryStockGroupDefOf.Medicine);
 			}
 			return int.MinValue;
 		}

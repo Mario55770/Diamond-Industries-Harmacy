6de7b214ab0b61a1554d3bd0980802156be9453e
Mario55770 <feretty@outlook.com>
2022-02-16 12:17:35 -0500

    Patch all weapons. Deleted test Items

--- 1.3/Comps/CompPoisonable.cs
+++ 1.3/Comps/CompPoisonable.cs
@@ -115,7 +115,7 @@ namespace DI_Harmacy
                 
                 //yield return new StatDrawEntry(StatCategoryDef.Weapon, "Weapon Inflicts", hediffToApply.Named());
                 yield return new StatDrawEntry(StatCategoryDefOf.Weapon, "Stat_Thing_ReloadChargesRemaining_Name".Translate(Props.ChargeNounArgument), LabelRemaining, "Stat_Thing_ReloadChargesRemaining_Desc".Translate(Props.ChargeNounArgument), 2749);
-                yield return new StatDrawEntry(StatCategoryDefOf.Weapon, "Poison Inflicts", hediffToApply.label, "This weapon applies this hediff", 2749);
+                yield return new StatDrawEntry(StatCategoryDefOf.Weapon, "Poison Inflicts", hediffToApply.label, "This weapon applies this hediff", 2750);
             }
         }
 

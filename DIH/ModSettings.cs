825b1cf6bb0799a17e23a421d411b1d9d501773c
Mario55770 <feretty@outlook.com>
2022-02-21 12:34:35 -0500

    Added Mod setting

--- DIH/ModSettings.cs
+++ DIH/ModSettings.cs
@@ -6,8 +6,9 @@ namespace DI_Harmacy
 {
         public class DIHSettings: ModSettings
         {
-            public static DIHSettings instance
+        public static DIHSettings instance;
             public bool debugLogging = false;
+        public float poisonMultiplier = 1;
             //public bool enableBulkRecipes=true;
 
             /// <summary>
@@ -21,15 +22,15 @@ namespace DI_Harmacy
             }
         }
 
-        public class QEEMod : Mod
+        public class DIHMod : Mod
         {
             /// <summary>
             /// A mandatory constructor which resolves the reference to our settings.
             /// </summary>
             /// <param name="content"></param>
-            public QEEMod(ModContentPack content) : base(content)
+            public DIHMod(ModContentPack content) : base(content)
             {
-                QEESettings.instance = base.GetSettings<QEESettings>();
+            DIHSettings.instance = base.GetSettings<DIHSettings>();
             }
 
             /// <summary>
@@ -40,19 +41,17 @@ namespace DI_Harmacy
             {
                 Listing_Standard listingStandard = new Listing_Standard();
                 listingStandard.Begin(inRect);
-                //listingStandard.SliderLabeled("QE_MaintenanceWorkThreshold".Translate(), ref QEESettings.instance.maintWorkThresholdFloat, QEESettings.instance.maintWorkThresholdFloat.ToStringPercent(), 0.00f, 1.0f, "QE_MaintenanceWorkThresholdTooltip".Translate());
-                
-                listingStandard.CheckboxLabeled("QE_DebugLogging".Translate(), ref QEESettings.instance.debugLogging, "QE_DebugLoggingTooltip".Translate());
-                listingStandard.End();
+            listingStandard.Label(label: "Poison Multiplier "+DIHSettings.instance.poisonMultiplier.ToString(), tooltip: "This is how much further poison should be affected by. Default value of 1. It is still multiplied by toxic sensitivity, a handful of other internal values, and for melee weapons, the strength of the hit");
+            DIHSettings.instance.poisonMultiplier = Mathf.Round(listingStandard.Slider(100f * DIHSettings.instance.poisonMultiplier, 0f, 200f)) / 100f;
+
+            //listingStandard.Slider("Poison effect multiplier", ref DIHSettings.instance.poisonMultiplier, DIHSettings.instance.poisonMultiplier.ToStringPercent(), 0.00f, 1.0f, "This is how much further poison should be affected by. Default value of 1. It is still multiplied by toxic sensitivity, a handful of other internal values, and for melee weapons, the strength of the hit");
+            listingStandard.End();
                 base.DoSettingsWindowContents(inRect);
             }
 
             public static void TryLog(string message)
             {
-                if (QEESettings.instance.debugLogging)
-                {
-                    Log.Message("QEE: " + message);
-                }
+               
             }
 
             /// <summary>
@@ -60,10 +59,9 @@ namespace DI_Harmacy
             /// Using .Translate() is optional, but does allow for localisation.
             /// </summary>
             /// <returns>The (translated) mod name.</returns>
-            public override string SettingsCategory() => "Questionable Ethics Enhanced";
+            public override string SettingsCategory() => "Diamond Industry Harmacy";
         }
 
 
-    }
-}
+    
 }
